b7c30edab6a3d9a03042fe2932e15fe49c529372
TamasSzekeres <szektam2@gmail.com>
2017-05-15 18:56:59 +0200

    add: error.cr

---
+++ src/asound/error.cr
@@ -0,0 +1,12 @@
+module ALSA
+  @[Link("asound")]
+  lib ASound
+    # Error handling
+
+    SND_ERROR_BEGIN = 500000 # Lower boundary of sound error codes.
+    SND_ERROR_INCOMPATIBLE_VERSION = (SND_ERROR_BEGIN + 0) # Kernel/library protocols are not compatible.
+    SND_ERROR_ALISP_NIL = (SND_ERROR_BEGIN + 1) # Lisp encountered an error during acall.
+
+    fun snd_strerror(errnum : Int32) : UInt8*
+  end
+end
